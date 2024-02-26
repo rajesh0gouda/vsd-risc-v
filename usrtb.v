618e41cd46459c2ab86a9e2809093a16dc80aa9f
rajesh0gouda <160471378+rajesh0gouda@users.noreply.github.com>
2024-02-27 00:14:22 +0530

    Create usrtb.v

---
+++ usrtb.v
@@ -0,0 +1,46 @@
+module unsrtb;
+
+reg [3:0]parin;
+reg clr;
+reg clk;
+reg [0:1]sel;
+wire [3:0]out;
+
+universal_shift_register uut(.parin(parin),.clr(clr),.clk(clk),.sel(sel),.out(out));
+  
+  initial begin
+  $dumpfile("dump.vcd");
+  
+  $dumpvars(1);
+end
+
+initial  begin 
+clk=0;
+repeat(100)
+#20 clk=~clk;
+end
+
+initial  begin
+parin=4'b1011;
+sel=2'b00;
+clr=1'b0;
+#40;
+  
+parin=4'b1011;
+sel=2'b01;
+clr=1'b0;
+#40;
+
+
+parin=4'b1011;
+sel=2'b10;
+clr=1'b0;
+#40;
+
+parin=4'b1011;
+sel=2'b11;
+clr=1'b0;
+#40;
+
+end
+endmodule
